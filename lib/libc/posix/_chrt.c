482f44c2378bb823507d1d92f1b12c9f470b87d5
bluaxe <dddd1022dddd@126.com>
2013-04-09 02:47:38 +0800

    add chrt but haven't add sys_chrt() yet

---
+++ lib/libc/posix/_chrt.c
@@ -0,0 +1,14 @@
+#include <lib.h>
+#define chrt _chrt
+#include <unistd.h>
+
+PUBLIC int chrt(deadline)
+_CONST long deadline;
+{
+  message m;
+
+  //TODO  -> add deadline to m
+  m.m4l1=deadline;
+
+  return(_syscall(PM_PROC_NR, CHRT, &m));
+}
